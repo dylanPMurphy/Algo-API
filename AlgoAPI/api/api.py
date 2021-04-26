d5eff063314784a74a09eff21298fd1e9a6d3360
Dylan Murphy <dylanmurphydba@gmail.com>
2021-04-25 22:45:41 -0700

    simple test first

--- AlgoAPI/api/api.py
+++ AlgoAPI/api/api.py
@@ -3,7 +3,5 @@ import . from get_valid_IP_addresses
 import json
 
 def isValidIP(request, input):
-    ipInQuestion = json.loads(input)
-    if isinstance(ipInQuestion, str):
-        return json.dumps(validIPAddresses(ipInQuestion))
+    return request
 
