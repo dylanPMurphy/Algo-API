e344c70fd0a9ab873fa2dbc787c0aec552ccf27c
Dylan Murphy <dylanmurphydba@gmail.com>
2021-04-25 22:51:13 -0700

    adding index to views

--- AlgoAPI/api/api.py
+++ AlgoAPI/api/api.py
@@ -2,8 +2,6 @@ from django.shortcuts import render, redirect, HttpResponse
 from . import get_valid_IP_addresses
 import json
 
-def index(request):
-    return HttpResponse("Success")
 def isValidIP(request, input):
     return request
 
