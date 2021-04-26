03b1016f2e01ef4ca76d6f428206314587daf378
Dylan Murphy <dylanmurphydba@gmail.com>
2021-04-25 22:58:05 -0700

    api

--- AlgoAPI/api/api.py
+++ AlgoAPI/api/api.py
@@ -2,6 +2,6 @@ from django.shortcuts import render, redirect, HttpResponse
 from . import get_valid_IP_addresses
 import json
 
-def isValidIP(request, input):
-    return request
+def isValidIP(request):
+    return request.POST['input']
 
